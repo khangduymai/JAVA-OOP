0f55e4429b19d33dc4bc1b966d7ae5aa2245e14d
khangduymai <khangmai4591@gmail.com>
2019-03-13 15:56:33 -0700

    update March 13, 2019 OOP

--- demo_InvoiceApp/src/InvoiceApp.java
+++ demo_InvoiceApp/src/InvoiceApp.java
@@ -7,7 +7,7 @@ public class InvoiceApp {
     public static void main(String args[]) {
         System.out.println("Welcome to the invoice application. \n");
         getLineItems();
-        // displayInvoice();
+        displayInvoice();
     }
 
     public static void getLineItems() {
@@ -16,8 +16,10 @@ public class InvoiceApp {
         while (choice.equalsIgnoreCase("y")) {
             System.out.print("Enter product code: ");
             String productCode = sc.nextLine();
+
             System.out.print("Enter quantity: ");
             int quantity = sc.nextInt();
+            sc.nextLine();
 
             Product product = ProductDB.getProduct(productCode);
             invoice.addItem(new LineItem(product, quantity));
@@ -26,20 +28,23 @@ public class InvoiceApp {
             choice = sc.nextLine();
 
         }
+
     }
 
     public static void displayInvoice() {
-        System.out.println("Code\tDescription\t\t\t\t\tPrice\tQty\t\t\tTotal");
-        System.out.println("----\t-----------\t\t\t\t\t------\t---\t\t\t-------");
+        System.out.println("Code\tDescription\t\t\tPrice\tQty\tTotal");
+        System.out.println("----\t-----------\t\t\t------\t---\t-------");
         for (LineItem lineItem : invoice.getLineItems()) {
             Product product = lineItem.getProduct();
-            String s = product.getCode() + "\t" + product.getDescription()
-                    + "\t\t" + product.getPrice() + "\t" + lineItem.getQuantity()
-                    + "\t\t\t" + lineItem.getTotal();
+            String s = product.getCode()
+                    + "\t" + product.getDescription()
+                    + "\t" + product.getPrice()
+                    + "\t" + lineItem.getQuantity()
+                    + "\t" + lineItem.getTotal();
             System.out.println(s);
 
-            System.out.println("\n\t\t\t\t\t\t\t\t\t Invoice total: \t" +
-                    invoice.getInvoiceTotal());
         }
+        System.out.println("\n\t\t\t\t\t\t\t\t\t Invoice total: \t" +
+                invoice.getInvoiceTotal());
     }
 }
